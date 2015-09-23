a4bddca325d09c13cbcdffdcae3448d8dd7de31f
James Dobson <james.dobson@onthebeach.co.uk>
2015-09-23 10:29:01 +0100

    Refactor

--- string_calculator/spec/string_calculator_spec.rb
+++ string_calculator/spec/string_calculator_spec.rb
@@ -1,69 +1,93 @@
 class StringCalculator
-
-  def initialize(string)
-    @string = string
-    @delimiter = @string[/^\/\/(.+)\n/m, 1].to_s
-    @delimiters = @delimiter.tr('[', '').split(']')
-  end
-
-  def add
-    negatives = []
-    result = @string.split(/[\n,#{Regexp.escape(@delimiter.to_s)}]/).inject(0) do |sum, num|
-      negatives << num.strip if num.to_i < 0
-      sum += num.to_i if num.to_i <= 1000
-      sum
+  def add(string)
+    negative_numbers = []
+    result = parse_numbers(string).inject(0) do |sum, number|
+      negative_numbers << number if number < 0
+      sum + number
     end
-    if negatives.size > 0
-      raise "Negatives not allowed (#{negatives.join(", ")})"
-    end
+    raise "Negatives not allowed (#{negative_numbers.join(", ")})" if negative_numbers.size > 0
+
     result
   end
 
+  private
+
+  def parse_numbers(string)
+    if custom_delimiter_match = /^\/\/(.+)\n/m.match(string)
+      string = string.sub(custom_delimiter_match[0], '')
+      custom_delimiter = custom_delimiter_match[1]
+    end
+
+    string.split(delimiter_regex(custom_delimiter)).map(&:to_i).reject { |number| number > 1000 }
+  end
+
+  def delimiter_regex(custom_delimiter)
+    delimiters = if custom_delimiter
+      custom_delimiter.scan(/\[?([^\[\]]+)\]?/).flatten.concat(default_delimiters)
+    else
+      default_delimiters
+    end
+
+    Regexp.union(delimiters.sort_by(&:length).uniq.reverse)
+  end
+
+  def default_delimiters
+    ["\n", ',']
+  end
 end
 
-RSpec.describe "string_calculator_text" do
-  it "returns 0 when an empty string is passed in" do
-    expect(StringCalculator.new("").add).to eq(0)
+RSpec.describe "String calculator" do
+  string_calculator = StringCalculator.new
+
+  describe "add" do
+    it "returns 0 when an empty string is passed in" do
+      expect(string_calculator.add("")).to eq(0)
+    end
+
+    it "takes a single number as a string and returns that number as an int" do
+      expect(string_calculator.add("1")).to eq(1)
+    end
+
+    it "takes a string of delimited numbers and returns the sum" do
+      expect(string_calculator.add("1,2")).to eq(3)
+    end
+
+    it "handles negative numbers" do
+      expect { string_calculator.add("10, 0, -42") }.to raise_error(/Negatives not allowed/)
+      expect { string_calculator.add("10, 0, -42") }.to raise_error("Negatives not allowed (-42)")
+    end
+
+    it "handles multiple negative numbers" do
+      expect{ string_calculator.add("-10, -90, -80, 8" )}.to raise_error(
+      "Negatives not allowed (-10, -90, -80)")
+    end
+
+    it "allows new lines to be a delimiter" do
+      expect(string_calculator.add("1\n2")).to eq(3)
+    end
+
+    it "allows you to configure your own delimiters" do
+      expect(string_calculator.add("//;\n1;2")).to eq(3)
+    end
+
+    it "ignores numbers that are greater than 1000" do
+      expect(string_calculator.add("2,1001")).to eq(2)
+    end
+
+    it "allows delimiters of any length" do
+      expect(string_calculator.add("//[***]\n1***2***3")).to eq(6)
+    end
+
+    it "allows multiple delimiters" do
+      expect(string_calculator.add("//[*][%]\n1*2%3")).to eq(6)
+    end
+
+    it "allows multiple delimiters of any length" do
+      expect(string_calculator.add("//[*%][%%%]\n1*%2%%%3")).to eq(6)
+    end
+
+    it "allows repeated delimiters" do
+      expect(string_calculator.add("//[*%][%%%][*%][*%*%]\n1*%2*%*%3")).to eq(6)
+    end
   end
-
-  it "takes a single number as a string and returns that number as an int" do
-    expect(StringCalculator.new("1").add).to eq(1)
-  end
-
-  it "parses a string for numbers" do
-    expect(StringCalculator.new("1,2").add).to eq(3)
-  end
-
-  it "handles negative numbers" do
-    calc = StringCalculator.new("10, 0, -42")
-
-    expect{calc.add}.to raise_error(/Negatives not allowed/)
-    expect{calc.add}.to raise_error("Negatives not allowed (-42)")
-  end
-
-  it "handles multiple negative numbers" do
-    expect{ StringCalculator.new("-10, -90, -80, 8" ).add}.to raise_error(
-    "Negatives not allowed (-10, -90, -80)")
-  end
-
-  it "allows new lines to be a delimiter" do
-    expect(StringCalculator.new("1\n2").add).to eq(3)
-  end
-
-  it "allows you to configure your own delimiters" do
-    expect(StringCalculator.new("//;\n1;2").add).to eq(3)
-  end
-
-  it "ignores numbers that are greater than 1000" do
-    expect(StringCalculator.new("2,1001").add).to eq(2)
-  end
-
-  it "allows delimiters of any length" do
-    expect(StringCalculator.new("//[***]\n1***2***3").add).to eq(6)
-  end
-
-  it "allows multiple delimiters" do
-    expect(StringCalculator.new("//[*][%]\n1*2%3").add).to eq(6)
-  end
-
 end
