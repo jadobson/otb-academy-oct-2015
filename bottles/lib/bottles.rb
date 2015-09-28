7d5dd86cce1ca1c59efc9f9c796220e1afdeb2bd
James Dobson <james.dobson@onthebeach.co.uk>
2015-09-28 14:28:54 +0100

    Refactor to use BottleNumber class

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,12 +1,54 @@
+class BottleNumber
+  def initialize(num)
+    @num = num
+  end
+
+  def decrement
+    return 99 if @num == 0
+    @num - 1
+  end
+
+  def no_more
+    @num == 0 ? "no more" : "#{@num}"
+  end
+
+  def bottles_remaining
+    if @num == 1
+      "no more bottles"
+    else
+      "#{decrement} #{bottles_or_bottle(@num - 1)}"
+    end
+  end
+
+  def it_or_one
+    @num == 1 ? "it" : "one"
+  end
+
+  def action
+    if @num == 0
+      "Go to the store and buy some more"
+    else
+      "Take #{it_or_one} down and pass it around"
+    end
+  end
+
+  def bottles_or_bottle(num = @num)
+    num == 1 ? "bottle" : "bottles"
+  end
+end
+
 class Bottles
   def verse(num)
-    string = "#{how_many_beers_on_the_wall(num).capitalize}, #{bottle_count(num)} of beer.\n"
-    string << action(num)
-    string << " #{how_many_beers_on_the_wall(next_count(num))}.\n"
+      "#{no_more(num).capitalize} #{bottles_or_bottle(num)} of beer on the wall, #{no_more(num)} #{bottles_or_bottle(num)} of beer.\n" +
+        "#{action(num)}, #{bottles_remaining(num)} of beer on the wall.\n"
   end
 
   def verses(num, num2)
-    num.downto(num2).collect { |v| self.verse(v) }.join("\n") + "\n"
+    result = ""
+    num.downto(num2).each do |v|
+      result += verse(v) + "\n"
+    end
+    result
   end
 
   def sing
@@ -14,28 +56,27 @@ class Bottles
   end
 
   private
-
   def action(num)
-    if num > 0
-      "Take #{bottle_descripter(num)} down and pass it around,"
-    else
-      "Go to the store and buy some more,"
-    end
+    BottleNumber.new(num).action
   end
 
-  def how_many_beers_on_the_wall(num)
-    "#{bottle_count(num)} of beer on the wall"
+  def bottles_or_bottle(num)
+    BottleNumber.new(num).bottles_or_bottle
   end
 
-  def bottle_descripter(num)
-    num == 1 ? "it" : "one"
+  def it_or_one(num)
+    BottleNumber.new(num).it_or_one
   end
 
-  def bottle_count(count)
-    { 0 => "no more bottles", 1 => "1 bottle" }.fetch(count, "#{count} bottles")
+  def bottles_remaining(num)
+    BottleNumber.new(num).bottles_remaining
   end
 
-  def next_count(count)
-    count == 0 ? 99 : count - 1
+  def decrement(num)
+    BottleNumber.new(num).decrement
+  end
+
+  def no_more(num)
+    BottleNumber.new(num).no_more
   end
 end
