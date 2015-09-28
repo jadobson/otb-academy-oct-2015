de6f24cef127f2473f88dbcb14f78e58786fb987
James Dobson <james.dobson@onthebeach.co.uk>
2015-09-28 15:34:45 +0100

    Refactor to use subclasses and factory

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,52 +1,73 @@
 class BottleNumber
+  def self.new(num)
+    if num == 0
+      object = BottleNumber0.allocate
+    elsif num == 1
+      object = BottleNumber1.allocate
+    else
+      object = BottleNumber.allocate
+    end
+
+    object.send(:initialize, num)
+    object
+  end
+
   def initialize(num)
     @num = num
   end
 
   def decrement
-    return 99 if none?
     @num - 1
   end
 
-  def no_more
-    none? ? "no more" : "#{@num}"
+  def action
+    "Take one down and pass it around"
   end
 
-  def it_or_one
-    last? ? "it" : "one"
+  def remaining
+    "#{@num}"
+  end
+
+  def pluralize
+    "bottles"
+  end
+
+  def next
+    @next_bottle_number ||= BottleNumber.new(decrement)
+  end
+end
+
+class BottleNumber0 < BottleNumber
+  def decrement
+    99
   end
 
   def action
-    if none?
-      "Go to the store and buy some more"
-    else
-      "Take #{it_or_one} down and pass it around"
-    end
+    "Go to the store and buy some more"
   end
 
-  def bottles_or_bottle
-    last? ? "bottle" : "bottles"
+  def remaining
+    "no more"
+  end
+end
+
+class BottleNumber1 < BottleNumber
+  def action
+    "Take it down and pass it around"
   end
 
-  private
-
-  def last?
-    @num == 1
-  end
-
-  def none?
-    @num == 0
+  def pluralize
+    "bottle"
   end
 end
 
 class Bottles
   def verse(num)
     bottle_number = BottleNumber.new(num)
-    next_bottle = BottleNumber.new(bottle_number.decrement)
 
-    "#{bottle_number.no_more.capitalize} #{bottle_number.bottles_or_bottle} of beer on the wall, "\
-    "#{bottle_number.no_more} #{bottle_number.bottles_or_bottle} of beer.\n"\
-    "#{bottle_number.action}, #{next_bottle.no_more} #{next_bottle.bottles_or_bottle} of beer on the wall.\n"
+    "#{bottle_number.remaining.capitalize} #{bottle_number.pluralize} of beer on the wall, "\
+    "#{bottle_number.remaining} #{bottle_number.pluralize} of beer.\n"\
+    "#{bottle_number.action}, #{bottle_number.next.remaining} #{bottle_number.next.pluralize} of beer on the wall.\n"
   end
 
   def verses(num, num2)
