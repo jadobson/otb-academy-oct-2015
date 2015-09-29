2a6b1410c930860562bb76a01a691f979cadbe0a
James Dobson <james.dobson@onthebeach.co.uk>
2015-09-29 10:34:12 +0100

    Tyre pressure alarm

--- tyre-pressure/lib/alarm.rb
+++ tyre-pressure/lib/alarm.rb
@@ -1,3 +1,19 @@
 class Alarm
-  # Fill in the implementation here
+  def initialize(sensor)
+  	@sensor = sensor
+  end
+
+  def on?
+  	@alarm 
+  end
+
+	def check
+  	@alarm = !in_range?(@sensor.sample_pressure)
+  end
+
+  private
+
+  def in_range?(pressure)
+  	(17.5..21).include?(pressure)
+  end
 end
--- tyre-pressure/spec/alarm_spec.rb
+++ tyre-pressure/spec/alarm_spec.rb
@@ -1,4 +1,35 @@
 require 'alarm'
+require 'sensor'
+
 RSpec.describe "tyre pressure alarm" do
-  it "needs some tests"
+	let(:sensor) { Sensor.new }
+  let(:alarm) { Alarm.new(sensor) }
+
+  it "triggers the alarm if the tyre pressure is below the normal range (17.5)" do
+  	allow(sensor).to receive(:sample_pressure) { 15.0 }
+  	alarm.check
+
+  	expect(alarm.on?).to eq(true)
+  end
+
+  it "triggers the alarm if the tyre pressure is above the normal range (21)" do
+   	allow(sensor).to receive(:sample_pressure) { 25.0 }
+  	alarm.check
+
+  	expect(alarm.on?).to eq(true)
+  end
+
+  it "does not trigger the alarm if the tyre pressure is within the normal range (17.5-21)" do
+   	allow(sensor).to receive(:sample_pressure) { 19.0 }
+  	alarm.check
+
+  	expect(alarm.on?).to eq(false)
+  end
+
+  it "does not trigger the alarm if the tyre pressure is on the edge of the allowed range" do
+   	allow(sensor).to receive(:sample_pressure) { 19.0 }
+  	alarm.check
+
+  	expect(alarm.on?).to eq(false)  		
+	end
 end
